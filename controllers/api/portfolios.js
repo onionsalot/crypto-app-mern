be4324d0b4180cfaef0e69292d5cbef5d31d9c16
onionsalot <trong.nguyen1008@gmail.com>
2021-06-25 16:39:06 -0400

    adds update and addcoin backend functionality

--- controllers/api/portfolios.js
+++ controllers/api/portfolios.js
@@ -3,6 +3,8 @@ const Portfolio = require("../../models/portfolio");
 module.exports = {
   index,
   create,
+  update,
+  addCoin,
 };
 
 async function index(req, res, next) {
@@ -18,4 +20,26 @@ async function create(req, res, next) {
   console.log(data)
   const createdPortfolio = await Portfolio.create(data);
   res.json(createdPortfolio)
+}
+
+async function update(req, res, next) {
+  // For general purpose updating such as name. If used for updating coins, will need to replace entire array of coins(!)
+  const url = req.params.id;
+  const data = req.body;
+  const updatedPortfolio = await Portfolio.findByIdAndUpdate(url, data, {new:true})
+  res.json(updatedPortfolio)
+}
+
+async function addCoin(req, res, next) {
+  try {
+    // Currently pushes to coin array but doesn't respond with updated info...
+    const id = req.params.id;
+    const cid = req.params.cid;
+    const addedCoin = await Portfolio.findById(id, async function(err, portfolio) {
+      portfolio.coins.push(cid);
+      portfolio.save();
+    }).then(response => res.json(response))
+  } catch(err) {
+    res.send(err)
+  }
 }
--- routes/api/portfolios.js
+++ routes/api/portfolios.js
@@ -6,5 +6,7 @@ const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 router.get('/', ensureLoggedIn, portfoliosCtrl.index);
 router.post('/create', ensureLoggedIn, portfoliosCtrl.create)
+router.put('/update/:id', ensureLoggedIn, portfoliosCtrl.update)
+router.put('/:id/add/:cid', ensureLoggedIn, portfoliosCtrl.addCoin)
 
 module.exports = router;
