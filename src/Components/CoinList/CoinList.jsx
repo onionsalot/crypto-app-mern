36d755fce81d9de36c55a1a7af1a1c9099619199
onionsalot <trong.nguyen1008@gmail.com>
2021-07-02 16:11:56 -0400

    finishes layout for index page

--- src/Components/CoinList/CoinList.jsx
+++ src/Components/CoinList/CoinList.jsx
@@ -1,29 +1,49 @@
 import CoinListItem from "../CoinListItem/CoinListItem";
-import Table from "react-bootstrap/Table";
+import { useMediaQuery } from "react-responsive";
 import "./CoinList.css";
 
 export default function CoinList({ coins }) {
   const coinItem = coins.map((coin, idx) => (
     <CoinListItem key={idx} coin={coin} />
   ));
-  // if (loading === true) {
-  //   return <div>Loading ...</div>
-  // }
-  // return (
+
+
+  const Large = ({ children }) => {
+    const isDesktop = useMediaQuery({ minWidth: 700 });
+    return isDesktop ? children : null;
+  };
+
+  const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 699 })
+    return isMobile ? children : null
+  }
+
+
   return (
     <>
       <div className="table-container">
         <table>
           <thead>
             <tr>
+              <Large>
+                <th className="head-col">Name</th>
+                <th>Ticker</th>
+                <th>Price</th>
+                <th>Market Cap</th>
+                <th>1hr</th>
+                <th>24hr</th>
+                <th>7d</th>
+                <th>14d</th>
+              </Large>
+              <Mobile>
               <th className="head-col">Name</th>
-              <th>Ticker</th>
-              <th>Price</th>
-              <th>Market Cap</th>
-              <th>1hr</th>
-              <th>24hr</th>
-              <th>7d</th>
-              <th>14d</th>
+                <th>Price</th>
+                <th>Market Cap</th>
+                <th>1hr</th>
+                <th>24hr</th>
+                <th>7d</th>
+                <th>14d</th> 
+              </Mobile>
             </tr>
           </thead>
           <tbody>{coinItem}</tbody>
--- src/Components/CoinListItem/CoinListItem.jsx
+++ src/Components/CoinListItem/CoinListItem.jsx
@@ -1,19 +1,43 @@
 import '../CoinList/CoinList.css'
 import { Link } from "react-router-dom"
+import { useMediaQuery } from "react-responsive";
 import star from "../../images/star.png"
 
 export default function CoinListItem({coin}) {
   const url = `details/${coin.id}/`
   const fav = `portfolio/add/${coin.id}`
+
+  const Large = ({ children }) => {
+    const isDesktop = useMediaQuery({ minWidth: 700 });
+    return isDesktop ? children : null;
+  };
+
+  const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 699 })
+    return isMobile ? children : null
+  }
+
+
 	return (
     <tr>
+      <Large>
         <td className="coin-name head-col">
-        <div className="coin-L"><Link to={fav}><img src={star} alt="fav" className="favicon"/></Link>{coin.rank}</div>
-          
-          <div className="coin-R"><Link to={url}><img src={coin.image} alt="coin" className="coinImage"/>{coin.name}</Link></div>
-          
+          <div className="coin-L"><Link to={fav}><img src={star} alt="fav" className="favicon"/></Link>{coin.rank}</div>
+            
+            <div className="coin-R"><Link to={url}><img src={coin.image} alt="coin" className="coinImage"/>{coin.name}</Link></div>
+            
         </td>
         <td> {coin.ticker} </td>
+      </Large>
+      <Mobile>
+        <td className="coin-name head-col mob-view">
+          <div className="rank-icon"><Link to={fav}><img src={star} alt="fav" className="favicon"/></Link>{coin.rank}</div>
+          <div className="coin-icon"><img src={coin.image} alt="coin" className="coinImage"/></div>  
+          <div className="coin-R"><Link to={url}>{coin.name}</Link></div>
+          <div className="ticker">&nbsp;{coin.ticker}&nbsp;</div>
+            
+        </td>
+      </Mobile>
         <td> {coin.price.toLocaleString()} </td>
         <td> {coin.market_cap.toLocaleString()} </td>
         <td className={coin.price1h >= 0? "green": "red"}> {coin.price1h ? (coin.price1h.toFixed(2)): (null)} </td>
